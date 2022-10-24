89db1fc71263da1a6b252639955c46032e9eebc8
Dean Gite <dgite@ritech.co>
2022-10-24 16:12:09 +0200

    create dataset navigation data

--- src/components/App.spec.tsx
+++ src/components/App.spec.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import "cross-fetch/polyfill";
-import { createLocalVue } from "@vue/test-utils";
+import { createLocalVue, mount, shallowMount } from "@vue/test-utils";
 import { render, waitFor } from "@testing-library/vue";
 import App from "./App";
 import { AppProps } from "@/types/components";
@@ -9,9 +9,12 @@ import createStore, { FSXAActions, RootState } from "@/store";
 import { FSXAContentMode, FSXAProxyRoutes, LogLevel } from "fsxa-api";
 import nock from "nock";
 import { getMockNavigationData } from "../../testing/getMockNavigationData";
-import { VueConstructor } from "vue";
+import { CreateElement, RenderContext, VNode, VueConstructor } from "vue";
 import { initializeApi } from "@/utils";
+import { initializeApp } from "@/store/actions/initializeApp";
 import { CreateStoreProxyOptions } from "@/types/fsxa-pattern-library";
+import { getMockDatasetData } from "../../testing/getMockDatasetData";
+import Dataset from "@/components/Dataset";
 
 const API_URL = "http://fsxa.local";
 
@@ -102,5 +105,30 @@ describe("App", () => {
 
     const app = renderApp({ store, localVue });
     return app.findByText("Resource could not be found", { exact: false });
+  });
+
+  it("should fail if current path doesnt exists for de_DE locale", async () => {
+    const { store, localVue } = setup();
+
+    const navigationData = getMockNavigationData();
+    const datasetData = getMockDatasetData();
+
+    const srv = await nock(API_URL)
+      .post(FSXAProxyRoutes.FETCH_NAVIGATION_ROUTE)
+      .reply(200, navigationData)
+      .post(FSXAProxyRoutes.FETCH_BY_FILTER_ROUTE)
+      .reply(200, datasetData);
+
+    const currentPath = "/Produkte/Stick-Up-Cam-Sicherheitskamera-FST-35J.html";
+
+    const wrapper = mount(App, {
+      store,
+      localVue,
+      propsData: {
+        currentPath,
+      },
+    });
+    // const dataSet = wrapper.findComponent(Dataset);
+    console.log(wrapper.html());
   });
 });
---
+++ testing/getMockDatasetData.ts
@@ -0,0 +1,61 @@
+import { Dataset } from "fsxa-api";
+
+export const getMockDatasetData = (): Dataset => ({
+  type: "Dataset",
+  id: "9346ff4e-9a3f-4e65-ab60-bf00e50cf98b",
+  previewId: "9346ff4e-9a3f-4e65-ab60-bf00e50cf98b.de_DE",
+  schema: "products",
+  entityType: "product",
+  data: {
+    tt_abstract:
+      "Behalten Sie Ihr Zuhause im Auge mit dem Stick Up Cam Akku. Befestigen Sie die Kamera an einer Wand oder einer ebenen Fläche im Innen- oder Außenbereich. Sehen Sie, wenn sich Personen nähern, mit bewegungsaktivierten Sofortwarnungen, 1080p HD-Video und Gegensprechanlage. Schließen Sie Ihre Stick Up Cam an Echo Show, Echo Spot, Fire TV oder ein Alexa-fähiges Feuertablett an, um Ihr Zuhause bequem von Ihrem Sofa aus zu überwachen. Möchten Sie einen Echtzeitüberblick über das Geschehen vor Ihrer Kamera? Fragen Sie einfach Alexa. ",
+    tt_categories: [[Object], [Object], [Object]],
+    tt_compatibility: [[Object], [Object]],
+    tt_delivery: [[Object]],
+    tt_installation: [[Object]],
+    tt_media: [[Object]],
+    tt_mode: {
+      type: "Option",
+      key: "4440a01d-c547-4a29-a672-633985a047bb",
+      value: "Solarbetrieb",
+    },
+    tt_name: "Stick Up Cam Sicherheitskamera FST-35J",
+    tt_permissions: {
+      type: "Permission",
+      fsType: "CMS_INPUT_PERMISSION",
+      name: "tt_permissions",
+      value: [Array],
+    },
+    tt_price: "189€",
+    tt_publish_at: null,
+    tt_relatedproducts: [],
+    tt_showrelatedproducts: {
+      fsType: "Option",
+      label: "KI basierte Empfehlungen anzeigen",
+      identifier: "ice",
+    },
+    tt_teaser_image: {
+      type: "Image",
+      id: "72c4dd5d-2266-4baa-a242-a350dc05e512",
+      previewId: "72c4dd5d-2266-4baa-a242-a350dc05e512.de_DE",
+      meta: {},
+      description: null,
+      resolutions: [Object],
+    },
+    tt_teaser_image_alt: null,
+    tt_teaser_text: "Intelligente Sicherheit jederzeit und überall!",
+  },
+  route: "/Produkte/Stick-Up-Cam-Sicherheitskamera-FST-35J.html",
+  routes: [
+    {
+      pageRef: "b1362730-4ee2-4473-bc02-529e47b08e95",
+      route: "/Produkte/Stick-Up-Cam-Sicherheitskamera-FST-35J.html",
+    },
+    {
+      pageRef: "9ffc3704-50f4-4e97-bfb4-3ac397e7784a",
+      route: "/Ueber-uns/CT-Test/9346ff4e-9a3f-4e65-ab60-bf00e50cf98b.html",
+    },
+  ],
+  template: "products.product",
+  children: [],
+});
