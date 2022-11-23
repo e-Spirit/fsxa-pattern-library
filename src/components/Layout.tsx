2142a990d3ea2e12e1a9eb532f8b3aef84b328ed
Dean Gite <dgite@ritech.co>
2022-11-23 10:37:26 +0100

    add extra condition for displayed property

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -196,8 +196,11 @@ class Layout<Data = {}, Meta = {}> extends RenderUtils<
     if (this.isEditMode) {
       // since object are passed by reference we need to create a new object
       const filteredContent: PageBody = JSON.parse(JSON.stringify(content));
+      // we filter either the children with displayed true or displayed is not set at all
       filteredContent.children = filteredContent.children.filter(
-        section => section.type === "Section" && !!section.displayed,
+        section =>
+          section.type === "Section" &&
+          [true, undefined].includes(section.displayed),
       );
       return filteredContent;
     }
