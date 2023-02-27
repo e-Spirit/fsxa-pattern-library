e130d5c702871fc056f49a0845f7beef7c0656fb
Dean Gite <dgite@ritech.co>
2023-02-27 12:01:52 +0100

    fix(layout): fix circular reference on content of layout component

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,10 +1,11 @@
 import { PageBody } from "fsxa-api";
+import { getCircularReplacer } from "./json-stringify";
 
-export const removeHiddenSections = (
-  content: PageBody,
-): PageBody => {
+export const removeHiddenSections = (content: PageBody): PageBody => {
   // since objects are passed by reference we need to create a new object
-  const filteredContent: PageBody = JSON.parse(JSON.stringify(content));
+  const filteredContent: PageBody = JSON.parse(
+    JSON.stringify(content, getCircularReplacer(), 2),
+  );
   // we filter either the children with displayed true or displayed is not set at all
   filteredContent.children = filteredContent.children.filter(
     section =>
